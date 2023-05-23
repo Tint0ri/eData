4c787f4b808a2d70e791670f10b27be6cf7a7b9f
Tint0ri <64587482+Tint0ri@users.noreply.github.com>
2023-05-23 13:28:13 +0800

    Add files via upload

---
+++ 首页.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="说明",
+)
+
+st.write("# 这是一个金融信息提供平台的演示")
+
+# st.sidebar.success("选择需要查询的信息")
+
+st.markdown(
+    """
+    ** 👈 请在左边的侧边栏选择 ** 想要查询的信息
+
+    ### 信息内容
+    - 沪深A股的行情信息
+    - 股票的公告信息
+    - 股票的研究报告
+
+    ### 说明
+    - 信息采集自网上，不保障实时性和准确性
+    - 仅作为非常初步的功能展示，存在bug和不完善请多包涵
+"""
+)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
